3bee7bedc4a1039ceef087e0ec8e49326ecf498a
YesidAros <yesid.aros@hotmail.com>
2025-05-28 17:50:08 -0500

    se agrega el menu en la parte inferior como fragment

---
+++ App_Sport/app/src/main/java/com/example/sport_app/Fragments/MenuBarFragment.kt
@@ -0,0 +1,19 @@
+package com.example.sport_app.Fragments
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.example.sport_app.R
+
+class MenuBarFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+
+        return inflater.inflate(R.layout.fragment_menu_bar, container, false)
+    }
+}
--- App_Sport/app/src/main/java/com/example/sport_app/Fragments/MenuFragment.kt
+++
@@ -1,7 +0,0 @@
-package com.example.sport_app.Fragments
-
-import androidx.fragment.app.Fragment
-
-class MenuFragment : Fragment(){
-
-}
